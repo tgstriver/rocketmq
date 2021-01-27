ad99f9d21c6a3e0ca0f159cdfddd9b71bfd1789c
striver <tgstriver@163.com>
2021-01-27 09:25:45 +0800

    add

--- broker/src/main/java/org/apache/rocketmq/broker/BrokerStartup.java
+++ broker/src/main/java/org/apache/rocketmq/broker/BrokerStartup.java
@@ -60,9 +60,7 @@ public class BrokerStartup {
 
     public static BrokerController start(BrokerController controller) {
         try {
-
             controller.start();
-
             String tip = "The broker[" + controller.getBrokerConfig().getBrokerName() + ", "
                 + controller.getBrokerAddr() + "] boot success. serializeType=" + RemotingCommand.getSerializeTypeConfigInThisServer();
 
